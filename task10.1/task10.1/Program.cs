5c622cd6c5e76c5f5b749ec04ac1c86a1548f42b
Лиза Ванеева <elivan2005@yahoo.com>
2024-10-01 10:04:05 +0500

    Задание 10.1 выполненоДобавьте файлы проекта.

---
+++ task10.1/task10.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число n");
+            int n;
+            if (!TryInputNumber(out n) || n <= 0)
+            {
+                Console.WriteLine("n должно быть натуральным числом.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Введите натуральное число k");
+            int k;
+            if (!TryInputNumber(out k) || k < 0)
+            {
+                Console.WriteLine("k должно быть неотрицательным целым числом.");
+                Console.ReadKey();
+                return;
+            }
+
+            double sum = 1;
+            for (int i = 1; i <= k; i++)
+            {
+                sum += 1.0 / Math.Pow(n, i);
+            }
+
+            Console.WriteLine($"Сумма выражения равна {sum}");
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(out int number)
+        {
+            number = 0;
+            if (!int.TryParse(Console.ReadLine(), out int n))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+            number = n;
+            return true;
+        }
+    }
+}
+
