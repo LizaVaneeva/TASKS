78c8af5ad48fdb7861040bd953b73251806a3c02
Лиза Ванеева <elivan2005@yahoo.com>
2024-10-01 10:15:28 +0500

    Задание 1

--- TASKS/Program.cs
+++ TASKS/Program.cs
@@ -27,18 +27,4 @@ internal class Program
         Console.WriteLine("Цвет текста возвращен в исходное состояние.");
         Console.ReadKey();
     }
-}using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TASKS
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
 }
