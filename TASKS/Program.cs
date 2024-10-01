08052c8cc0776b990f8e432ba08c6ff80ba821a3
Лиза Ванеева <elivan2005@yahoo.com>
2024-10-01 11:12:47 +0500

    Задание 3

--- TASKS/Program.cs
+++ TASKS/Program.cs
@@ -10,6 +10,26 @@ namespace TASKS
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите трехзначное число:");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number >= 100 && number <= 999)
+            {
+                var units = number % 10;        
+                var tenths = (number / 10) % 10; 
+                var hundreds = number / 100;      
+
+                var reversedNumber = units * 100 + tenths * 10 + hundreds;
+
+                Console.WriteLine($"Число с измененным порядком цифр: {reversedNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введите корректное трехзначное число.");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
