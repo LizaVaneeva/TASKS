99d3642e2a0cb5b254fea93d35826dc38b3bea36
Лиза Ванеева <elivan2005@yahoo.com>
2024-10-09 14:45:26 +0500

    Задание 4

---
+++ task4/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+            var x = double.Parse(Console.ReadLine());
+
+            var y = MyFunction(x);
+            Console.WriteLine("f(x) = " + y);
+
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            //throw new NotImplementedException();
+            return (x + ((2 + Math.Cos(x)) / (x * x))) / (x + (1 / Math.Sqrt(x * x + 10)));
+        }
+    }
+}
